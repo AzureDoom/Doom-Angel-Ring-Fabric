29cb3a888b22827fdc80ccd3676cfc1705bd320c
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-10-31 00:57:56 -0400

    Cleaned up code.

--- src/main/java/mod/azure/doomangelring/AngelRingItem.java
+++ src/main/java/mod/azure/doomangelring/AngelRingItem.java
@@ -1,8 +1,5 @@
 package mod.azure.doomangelring;
 
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
-
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
@@ -17,98 +14,100 @@ import top.theillusivec4.curios.api.CuriosApi;
 import top.theillusivec4.curios.api.CuriosCapability;
 import top.theillusivec4.curios.api.type.capability.ICurio;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 public class AngelRingItem extends Item {
 
-	private int damageTicks;
+    private int damageTicks;
 
-	public AngelRingItem() {
-		super(new Item.Properties().stacksTo(1).durability(DoomAngelRing.config.max_ring_durability));
-	}
+    public AngelRingItem() {
+        super(new Item.Properties().stacksTo(1).durability(DoomAngelRing.config.max_ring_durability));
+    }
 
-	@Override
-	public boolean isFoil(ItemStack stack) {
-		return false;
-	}
+    @Override
+    public boolean isFoil(ItemStack stack) {
+        return false;
+    }
 
-	@Override
-	public boolean isValidRepairItem(ItemStack stack, ItemStack ingredient) {
-		return ingredient.is(DoomAngelRing.RING_REPAIR);
-	}
+    @Override
+    public boolean isValidRepairItem(ItemStack stack, ItemStack ingredient) {
+        return ingredient.is(DoomAngelRing.RING_REPAIR);
+    }
 
-	@Override
-	public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundTag unused) {
-		final ICurio curio = new ICurio() {
-			@Override
-			public boolean canRightClickEquip() {
-				return true;
-			}
+    @Override
+    public ICapabilityProvider initCapabilities(final ItemStack stack, CompoundTag unused) {
+        final ICurio curio = new ICurio() {
+            @Override
+            public boolean canRightClickEquip() {
+                return true;
+            }
 
-			@Override
-			public void onEquip(String identifier, int index, LivingEntity livingEntity) {
-				if (livingEntity instanceof Player player) {
-					startPowers(player);
-				}
-			}
+            @Override
+            public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+                if (livingEntity instanceof Player player) {
+                    startPowers(player);
+                }
+            }
 
-			@Override
-			public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
-				if (livingEntity instanceof Player player)
-					stopPowers(player);
-			}
+            @Override
+            public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+                if (livingEntity instanceof Player player)
+                    stopPowers(player);
+            }
 
-			private void startPowers(Player player) {
-				if (!player.isCreative() && !player.isSpectator() && !player.onGround()) {
-					player.getAbilities().flying = true;
-					player.onUpdateAbilities();
-					if (player instanceof ServerPlayer serverplayer)
-						if (!serverplayer.onGround()) {
-							damageTicks++;
-							if (damageTicks >= DoomAngelRing.config.ticks_until_damage) {
-								stack.hurt(DoomAngelRing.config.ring_damage_on_tick, serverplayer.getRandom(), serverplayer);
-								damageTicks = 0;
-							}
-						}
-				}
-			}
+            private void startPowers(Player player) {
+                if (!player.isCreative() && !player.isSpectator() && !player.onGround()) {
+                    player.getAbilities().flying = true;
+                    player.onUpdateAbilities();
+                    if (player instanceof ServerPlayer serverplayer && !serverplayer.onGround()) {
+                        damageTicks++;
+                        if (damageTicks >= DoomAngelRing.config.ticks_until_damage) {
+                            stack.hurt(DoomAngelRing.config.ring_damage_on_tick, serverplayer.getRandom(), serverplayer);
+                            damageTicks = 0;
+                        }
+                    }
+                }
+            }
 
-			private void stopPowers(Player player) {
-				if (!player.isCreative() && !player.isSpectator()) {
-					player.getAbilities().flying = false;
-					player.getAbilities().flying = false;
-					player.onUpdateAbilities();
-				}
-			}
+            private void stopPowers(Player player) {
+                if (!player.isCreative() && !player.isSpectator()) {
+                    player.getAbilities().flying = false;
+                    player.getAbilities().flying = false;
+                    player.onUpdateAbilities();
+                }
+            }
 
-			@Override
-			public void curioTick(String identifier, int index, LivingEntity livingEntity) {
-				if (livingEntity instanceof Player player)
-					startPowers(player);
-			}
+            @Override
+            public void curioTick(String identifier, int index, LivingEntity livingEntity) {
+                if (livingEntity instanceof Player player)
+                    startPowers(player);
+            }
 
-			@Override
-			public boolean canEquip(String identifier, LivingEntity entityLivingBase) {
-				return !CuriosApi.getCuriosHelper().findFirstCurio(entityLivingBase, DoomAngelRing.ANGEL_RING.get()).isPresent();
-			}
+            @Override
+            public boolean canEquip(String identifier, LivingEntity entityLivingBase) {
+                return !CuriosApi.getCuriosHelper().findFirstCurio(entityLivingBase, DoomAngelRing.ANGEL_RING.get()).isPresent();
+            }
 
-			@Override
-			public ItemStack getStack() {
-				return new ItemStack(DoomAngelRing.ANGEL_RING.get());
-			}
-		};
+            @Override
+            public ItemStack getStack() {
+                return new ItemStack(DoomAngelRing.ANGEL_RING.get());
+            }
+        };
 
-		return new ICapabilityProvider() {
-			private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
+        return new ICapabilityProvider() {
+            private final LazyOptional<ICurio> curioOpt = LazyOptional.of(() -> curio);
 
-			@Nonnull
-			@Override
-			public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+            @Nonnull
+            @Override
+            public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
 
-				return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
-			}
-		};
-	}
+                return CuriosCapability.ITEM.orEmpty(cap, curioOpt);
+            }
+        };
+    }
 
-	public static boolean isRingInCuriosSlot(ItemStack belt, LivingEntity player) {
-		return CuriosApi.getCuriosHelper().findFirstCurio(player, belt.getItem()).isPresent();
-	}
+    public static boolean isRingInCuriosSlot(ItemStack belt, LivingEntity player) {
+        return CuriosApi.getCuriosHelper().findFirstCurio(player, belt.getItem()).isPresent();
+    }
 }
