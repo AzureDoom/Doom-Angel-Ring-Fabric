5d3bb375bc19f28c5513966891df7a9ea8a34b1d
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2023-12-18 01:23:35 -0500

    Update NeoForgeMod.java

--- neoforge/src/main/java/mod/azure/doomangelring/NeoForgeMod.java
+++ neoforge/src/main/java/mod/azure/doomangelring/NeoForgeMod.java
@@ -18,12 +18,13 @@ import net.minecraftforge.registries.RegistryObject;
 public final class NeoForgeMod {
     public static NeoForgeMod instance;
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, CommonMod.MOD_ID);
-    public static final RegistryObject<Item> ANGEL_RING = ITEMS.register("angelring", () -> new AngelRingItem());
+    public static final RegistryObject<Item> ANGEL_RING = ITEMS.register("angelring", AngelRingItem::new);
 
     public NeoForgeMod() {
         instance = this;
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
-        CommonMod.config = AzureLibMod.registerConfig(DoomAngelRingConfig.class, ConfigFormats.json()).getConfigInstance();
+        CommonMod.config = AzureLibMod.registerConfig(DoomAngelRingConfig.class,
+                ConfigFormats.json()).getConfigInstance();
         ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
